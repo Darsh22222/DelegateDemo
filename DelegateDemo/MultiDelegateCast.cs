adc3cdc73e63a32a9fafa8b586b489e6989a67bd
Darsh <darshankondhare2698@gmail.com>
2022-11-28 15:05:48 +0530

    [darsh] Add: Delegate

---
+++ DelegateDemo/MultiDelegateCast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    internal class MultiDelegateCast
+    {
+        public delegate int DelOp(int x, int y);
+        public static void ImplemrntDeligate()
+        {
+            DelOp obj = Operation.Add;
+            obj += Operation.Square;
+            obj(5, 5);
+            obj(8, 3);
+        }
+        public class Operation
+        {
+            public static int Add(int a, int b)
+            {
+                Console.WriteLine("Addition = {0}", a+b);
+                return (a + b);
+            }
+            public static int Square(int a, int b)
+            {
+                Console.WriteLine("Multiple = {0}", a + b);
+                return (a + b);
+            }
+        }
+    }
+}
---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateDemo
+{
+    public delegate int operation(int a, int b);
+
+    internal class Program
+    {
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            operation obj = new operation(Addition);
+            Console.WriteLine("Add is = {0}", obj(23, 22));
+            MultiDelegateCast.ImplemrntDeligate();
+        }
+    }
+}
